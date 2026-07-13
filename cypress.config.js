b42122e00ca9bdae0a78a64390d752830605e14b
Mariana Denarde <rapatoni73@gmail.com>
2026-07-13 18:49:22 -0300

    chore: Configuração inicial do projeto

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://front.serverest.dev",
+    specPattern: "cypress/e2e/**/*.cy.js",
+    supportFile: "cypress/support/e2e.js",
+    setupNodeEvents(on, config) {
+      return config;
+    },
+  },
+
+  env: {
+    apiUrl: "https://serverest.dev",
+  },
+
+  video: false,
+  defaultCommandTimeout: 8000,
+});
